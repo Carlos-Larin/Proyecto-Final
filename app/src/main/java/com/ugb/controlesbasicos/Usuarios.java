781d15d057e4fef947fd262257710829f5a221ea
Carlos-Larin <nekokar701@gmail.com>
2024-05-03 14:26:11 -0600

    Proyecto final en Firebase
    hecho por Carlos Larin, Briseyli Solorzano, Tania del Carmen, Josue Roberto and Angel Sorto.

---
+++ app/src/main/java/com/ugb/controlesbasicos/Usuarios.java
@@ -0,0 +1,6 @@
+package com.ugb.controlesbasicos;
+
+import android.app.Activity;
+
+public class Usuarios extends Activity {
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript{
+    repositories{
+        google()
+        mavenCentral()
+    }
+    dependencies{
+        classpath ("com.google.gms:google-services:4.3.15")
+    }
+}
 plugins {
     alias(libs.plugins.androidApplication) apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
