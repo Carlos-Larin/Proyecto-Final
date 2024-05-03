e74359b560e12883fd4931a2cb81230071c43959
Carlos-Larin <nekokar701@gmail.com>
2024-05-03 14:26:11 -0600

    Proyecto final en Firebase
    hecho por Carlos Larin, Briseyli Solorzano, Tania del Carmen, Josue Roberto and Angel Sorto.

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -8,19 +8,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import org.json.JSONObject;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    FloatingActionButton Fak;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
+
 }
---
+++ app/src/main/java/com/ugb/controlesbasicos/MyReceiverCALL.java
@@ -0,0 +1,12 @@
+package com.ugb.controlesbasicos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiverCALL extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
